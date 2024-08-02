03f13bde51d7b61557e71b690c95bd361a7b08fb
koit <koit3572@gmail.com>
2024-08-03 00:45:42 +0900

    2024-08-02

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,27 +6,31 @@ export interface IMainSideBarData {
   [dirName: string]: string[] ;
 }
 export const GET = async () => {
-  const postFolderStructure = getPostFolderStructure();
-  const postPaths = getAllPostPaths()!;
-  const posts = postPaths.reduce((acc, postPath) => {
-    const formatPath = postPath.join('/')
-    const postData = getPostData(formatPath)
-    return (acc = {
-      ...acc,
-      [formatPath]: {
-        title: postData.data.title,
-        writer: "koit",
-        createdAt: postData.data.createdAt,
-        updatedAt: postData.data.updatedAt,
-        discription: postData.data.discription,
-        tags: postData.data.tags,
-        isFavorite: postData.data.isFavorite,
-      } as PostData,
-    });
-  }, {})
-  const postData = {
-    postFolderStructure: postFolderStructure,
-    posts:posts
-  };
-  return NextResponse.json(postData);
+  try {
+    const postFolderStructure = getPostFolderStructure();
+    const postPaths = getAllPostPaths()!;
+    const posts = postPaths.reduce((acc, postPath) => {
+      const formatPath = postPath.join("/");
+      const postData = getPostData(formatPath);
+      return (acc = {
+        ...acc,
+        [formatPath]: {
+          title: postData.data.title,
+          writer: "koit",
+          createdAt: postData.data.createdAt,
+          updatedAt: postData.data.updatedAt,
+          discription: postData.data.discription,
+          tags: postData.data.tags,
+          isFavorite: postData.data.isFavorite,
+        } as PostData,
+      });
+    }, {});
+    const postData = {
+      postFolderStructure: postFolderStructure,
+      posts: posts,
+    };
+    return NextResponse.json(postData);
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
