8b53978cac6f5e70746fe4e76bb3d6607b801557
koit <koit3572@gmail.com>
2024-08-19 10:49:27 +0900

    2024-08-19

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   transpilePackages:[
     'gsap'
-  ]
+  ],
+  experimental:{
+    workerThreads:false,
+    cpus:1,
+  }
 }
 
 module.exports = nextConfig
